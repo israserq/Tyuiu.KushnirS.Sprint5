ecfc488c0d8eb0d1e9427513720dcff5cdc99a46
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-12-12 20:37:28 +0300

    Task7

---
+++ Tyuiu.KushnirS.Sprint5.Task7.V2.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.KushnirS.Sprint5.Task7.V2.Lib
+{
+    public class DataService : ISprint5Task7V2
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string resPath = Path.Combine(Path.GetTempPath(), "OutPutFileTask7V2.txt");
+
+            FileInfo fileInfo = new FileInfo(resPath);
+            bool fileExist = fileInfo.Exists;
+
+            if (fileExist) { File.Delete(resPath); }
+
+            string text = File.ReadAllText(path);
+
+            foreach (char c in text)
+            {
+                if (char.IsDigit(c))
+                {
+                    text = text.Replace(c, '#');
+                }
+            }
+
+            File.AppendAllText(resPath, text);
+
+            return resPath;
+
+        }
+    }
+}
---
+++ Tyuiu.KushnirS.Sprint5.Task7.V2/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.KushnirS.Sprint5.Task7.V2.Lib;
+namespace Tyuiu.KushnirS.Sprint5.Task7.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            DataService ds = new DataService();
+
+            
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask7V2.txt";
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"* Файл: {ds.LoadDataAndSave(path)} ");
+            Console.WriteLine($"* Создан!                                                       *");
+            Console.WriteLine("*****************************************************************");
+            Console.ReadKey();
+        }
+    }
+}
