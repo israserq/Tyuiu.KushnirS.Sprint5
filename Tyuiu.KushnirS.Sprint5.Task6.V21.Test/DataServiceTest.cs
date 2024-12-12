a6e2b0f7d85e6d524b1e5cdb84ef618052799a6d
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-12-12 20:28:18 +0300

    Task6

---
+++ Tyuiu.KushnirS.Sprint5.Task6.V21.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.KushnirS.Sprint5.Task6.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.KushnirS.Sprint5.Task6.V21/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.KushnirS.Sprint5.Task6.V21.Lib;
+namespace Tyuiu.KushnirS.Sprint5.Task6.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИССХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("***************************************************************************");
+
+
+            string path = @"C:\Users\Sergey\source\repos\Tyuiu.KushnirS.Sprint5\DataSprint5\InPutDataFileTask6V21.txt";
+
+            Console.WriteLine("Данные находяться в файле: " + path);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
