a8da325c5354e060351278cf4fb80a4108535e1e
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-12-12 19:07:15 +0300

    Add project files.

---
+++ Tyuiu.KushnirS.Sprint5.Task0.V22.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.KushnirS.Sprint5.Task0.V22.Lib
+{
+    public class DataService : ISprint5Task0V22
+    {
+        public string SaveToFileTextData(int x)
+        {
+            //$@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt"
+            string path = Path.GetTempFileName();
+            double z = (Math.Pow((1 - x), 2)) / (-3 * x);
+            z = Math.Round(z, 3);
+            File.WriteAllText(path, Convert.ToString(z));
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.KushnirS.Sprint5.Task0.V22/Program.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.KushnirS.Sprint5.Task0.V22.Lib;
+namespace Tyuiu.KushnirS.Sprint5.Task0.V22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+           
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int x = 3;
+            Console.WriteLine("х = " + x);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadLine();
+        }
+    }
+}
