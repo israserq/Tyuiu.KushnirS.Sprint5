eaea0d9e53569e4622b6b0acbefd3cfadeb3a969
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-12-12 20:11:45 +0300

    Task1

---
+++ Tyuiu.KushnirS.Sprint5.Task1.V4.Lib/DataService.cs
@@ -0,0 +1,45 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.KushnirS.Sprint5.Task1.V4.Lib
+{
+    public class DataService : ISprint5Task1V4
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask1.txt");
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileexist = fileInfo.Exists;
+
+            if (fileexist)
+            {
+                File.Delete(path);
+            }
+
+            double y;
+            string stry;
+
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (x + 1 == 0)
+                {
+                    y = 0;
+                    stry = Convert.ToString(y);
+                }
+                else
+                {
+                    y = Math.Round((Math.Cos(x) / (x + 1)) - Math.Cos(x) * 1.3 + 3 * x, 2);
+                    stry = Convert.ToString(y);
+                }
+                if (x != stopValue)
+                {
+                    File.AppendAllText(path, stry + Environment.NewLine);
+                }
+                else
+                {
+                    File.AppendAllText(path, stry);
+                }
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.KushnirS.Sprint5.Task1.V4/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.KushnirS.Sprint5.Task1.V4.Lib;
+namespace Tyuiu.KushnirS.Sprint5.Task1.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+            
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("startValue = " + startValue);
+            Console.WriteLine("stopValue = " + stopValue);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = ds.SaveToFileTextData(startValue, stopValue);
+
+            Console.WriteLine("Файл: " + path);
+            Console.WriteLine("Создан!");
+            Console.WriteLine(File.ReadAllText(path));
+        }
+    }
+}
