aba0086f9bc857cfb9826c0550fe787230e28421
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-12-12 20:22:05 +0300

    Task4

---
+++ Tyuiu.KushnirS.Sprint5.Task4.V4.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.KushnirS.Sprint5.Task4.V4.Lib
+{
+    public class DataService : ISprint5Task4V4
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string n = File.ReadAllText(path);
+            double x = double.Parse(n.Replace('.', ','));
+            double res = Math.Round(((0.25 * x) / (Math.Cos(x))), 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.KushnirS.Sprint5.Task4.V4/Program.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.KushnirS.Sprint5.Task4.V4.Lib;
+namespace Tyuiu.KushnirS.Sprint5.Task4.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            DataService ds = new DataService();
+            string path = Path.Combine(Path.GetTempPath(), "DataSprint5", "InPutDataFileTask4V4.txt");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
